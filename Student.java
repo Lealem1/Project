b297c4fd640f344decf673c644779de4647824e5
Lealem1 <Lealemdegie@gmail.com>
2023-11-26 15:00:24 +0300

    this is week4 project

--- Student.java
+++ Student.java
@@ -2,7 +2,7 @@ class Student extends Person {
     private Classroom classroom;
 
     //here is student class
-    public Student(String name, int age, boolean parent_permission, String classroom) {
+    public Student(String name, int age, boolean parent_permission) {
         super(name, age,parent_permission);
 
 
