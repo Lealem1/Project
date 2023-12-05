94453467c359d6ade126fd3e13cd38543890d052
Lealem1 <147622511+Lealem1@users.noreply.github.com>
2023-12-05 11:05:58 +0300

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,38 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 public class Book {
-    private String title;
-    private String author;
-    private List<Rental> rentals;
-
+    String title;
+     String author;
+     List<Rental> rentals;
     public Book(String title,String author) {
         this.title = title;
         this.author = author;
         this.rentals = new ArrayList<>();
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getAuthor() {
         return author;
     }
-
     public void setAuthor(String author) {
         this.author = author;
     }
-
     public List<Rental> getRentals() {
         return rentals;
     }
-
     public void addRental(Rental rental) {
         rentals.add(rental);
-
     }
 }
